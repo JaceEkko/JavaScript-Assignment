00243f59ca7d6f86a9429cac88d3c7e4f4daf704
John Knox <jgk305@nyu.edu>
2019-11-03 11:49:38 -0500

    script that compares string equality

---
+++ 01-stringCompare.js
@@ -0,0 +1,11 @@
+function compareString(){
+  var string1 = document.getElementById("01-var1").value
+  var string2 = document.getElementById("01-var2").value
+
+  if(string1 == string2){
+    alert("the two strings are equal!!")
+  }
+  else{
+    alert("the two strings are NOT equal!!")
+  }
+}
