5cdcf72ad70fb9a432d5d05941fc17c3679fe139
John Knox <jgk305@nyu.edu>
2019-11-03 11:51:21 -0500

    script that takes in ints that print different strings

---
+++ 04-functionCondition.js
@@ -0,0 +1,21 @@
+function dogBark(){
+  var bark = document.getElementById("04-var").value
+
+  //What the dog says (when 1 - 4 are inputted)
+  if(bark == 1){
+    console.log("The dog says 'BARK!'");
+  }
+  else if(bark == 2){
+    console.log("The dog says 'ARF!'");
+  }
+  else if(bark == 3){
+    console.log("The dog says 'WOOF!'");
+  }
+  else if(bark == 4){
+    console.log("The dog says 'BOW-WOW!'");
+  }
+  //When another number is inputted
+  else{
+    console.log("The dog...Doesn't know what to say :(");
+  }
+}
